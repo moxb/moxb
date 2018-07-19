6378597a7285d1748f045ff837323c08f5b5350e
Stefan Herndlbauer <sherndlbauer@gorillabyte.com>
2018-07-19 14:53:34 +0200

    Setup a basic react app with all dependencies
    I switched from the tsc compilation to webpack, because the browser
    target is far simpler to build.

---
+++ examples/semui/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class Application extends React.Component {
+    render() {
+        return <div>TEST</div>;
+    }
+}
+
+ReactDOM.render(<Application />, document.getElementById('example-app'));
