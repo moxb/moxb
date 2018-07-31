1b297ea572e4a3b7faff42cf089c6dc93d000098
Michael Scharf <github@scharf.gr>
2018-07-31 14:56:44 +0200

    Added sorting

--- examples/semui/memtable/MemTableImpl.ts
+++ examples/semui/memtable/MemTableImpl.ts
@@ -1,6 +1,7 @@
 import * as moxb from '@moxb/moxb';
 import { MemTable, MemTableData } from './MemTable';
 import { computed } from 'mobx';
+import { SortColumn } from '../../../packages/moxb/src/table/TableSort';
 
 const firstNames = [
     'James',
@@ -84,6 +85,26 @@ function createData(n: number): MemTableData[] {
     return result;
 }
 
+function sort(data: any[], sortOrder: SortColumn[]) {
+    if (sortOrder.length) {
+        data.sort((a, b) => {
+            for (let i = 0; i < sortOrder.length; i++) {
+                const order = sortOrder[i];
+                const mul = order.sortDirection === 'descending' ? 1 : -1;
+                const x = a[order.column];
+                const y = b[order.column];
+                if (x < y) {
+                    return mul;
+                }
+                if (x > y) {
+                    return -mul;
+                }
+            }
+            return 0;
+        });
+    }
+}
+
 export class MemTableImpl implements MemTable {
     readonly rows: moxb.Numeric = new moxb.NumericImpl({
         id: 'memtable.rows',
@@ -94,7 +115,7 @@ export class MemTableImpl implements MemTable {
         columns: table => [
             new moxb.TableColumnImpl(
                 {
-                    id: 'emails',
+                    id: 'email',
                     label: 'E-Mail',
                     preferredSortDirection: 'ascending',
                 },
@@ -118,13 +139,23 @@ export class MemTableImpl implements MemTable {
             ),
             new moxb.TableColumnImpl(
                 {
-                    id: 'createdAt',
+                    id: 'joined',
                     label: 'Joined',
                     preferredSortDirection: 'descending',
                 },
                 table
             ),
         ],
-        data: () => createData(this.rows.value || 0),
+        data: () => this.data,
     });
+    @computed
+    get data() {
+        const data = [...this.rawData];
+        sort(data, this.table.sort.sort);
+        return data;
+    }
+    @computed
+    get rawData() {
+        return createData(this.rows.value || 0);
+    }
 }
