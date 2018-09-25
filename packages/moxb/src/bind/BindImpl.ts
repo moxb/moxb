27a864c8e6a2d18ac81a7f021f4ddf77c412e36a
Stefan Herndlbauer <sherndlbauer@gorillabyte.com>
2018-09-25 17:17:52 +0200

    Refactor idToDomId in new utils function

--- packages/moxb/src/bind/BindImpl.ts
+++ packages/moxb/src/bind/BindImpl.ts
@@ -1,6 +1,7 @@
 import { action, computed, observable } from 'mobx';
 import { t } from '../i18n/i18n';
 import { bindAllTo } from '../util/bindAllTo';
+import { idToDomId } from '../util/idToDomId';
 import { Bind } from './Bind';
 
 export type ValueOrFunction<T> = T | (() => T | undefined) | undefined;
@@ -11,22 +12,6 @@ export function getValueOrFunction<T>(value: ValueOrFunction<T>): T | undefined 
     } else {
         return value;
     }
-}
-
-/**
- * Converts a string to a css id. Note that ids and classes in css are case insensitive, and therefore we convert
- * camel case into lower cases.
- * @param {string | null | undefined} id
- * @returns {string}
- */
-export function idToDomId(id: string | null | undefined) {
-    return (id || '')
-        .replace(/[^-\w\d]+/g, '-')
-        .replace(/\.?([A-Z]+)/g, (x, v) => '_' + v.toLowerCase()) // to camel case
-        .replace(/^([\d_-]+)/, '') // start with alpha numeric
-        .replace(/[-_]{2,}/g, '-') // no double dashes and _-
-        .replace(/^[-_]/g, '') // no _ - at the beginning
-        .replace(/[-_]$/g, ''); // no _ - at the end
 }
 
 export type StringOrFunction = ValueOrFunction<string>;
--- packages/moxb/src/bind/__tests__/BindImpl.test.ts
+++ packages/moxb/src/bind/__tests__/BindImpl.test.ts
@@ -1,6 +1,6 @@
 import { setTFunction, t, translateKeysDefault, translateKeysOnly } from '../../i18n/i18n';
 import { Bind } from '../Bind';
-import { BindImpl, BindOptions, getValueFromStringOrFunction, idToDomId } from '../BindImpl';
+import { BindImpl, BindOptions, getValueFromStringOrFunction } from '../BindImpl';
 
 describe('interface Bind', function() {
     function newBind(options: BindOptions) {
@@ -549,43 +549,5 @@ describe('interface Bind', function() {
         it('should return null', function() {
             expect(getValueFromStringOrFunction(() => undefined)).toBeUndefined();
         });
-    });
-});
-
-describe('idToDomId', function() {
-    it('should return empty string if id is empty', function() {
-        expect(idToDomId('')).toBe('');
-    });
-    it('should return empty string if id is null or undefined', function() {
-        expect(idToDomId(null)).toBe('');
-        expect(idToDomId(undefined)).toBe('');
-    });
-    it('should convert camel case to lowercase underscore', function() {
-        expect(idToDomId('TheId')).toBe('the_id');
-        expect(idToDomId('TheIDBar')).toBe('the_idbar');
-        expect(idToDomId('notIBM')).toBe('not_ibm');
-    });
-    it('should prepend `x` if first character is not word', function() {
-        expect(idToDomId('3xxx')).toBe('xxx');
-        expect(idToDomId('_a')).toBe('a');
-    });
-    it('should replace non word or number or `-` to `-`', function() {
-        expect(idToDomId('*-fo(ba^5$3BaX')).toBe('fo-ba-5-3_ba_x');
-    });
-    it('should not allow double -', function() {
-        expect(idToDomId('---a*-*--*b()----c------d')).toBe('a-b-c-d');
-    });
-    it('should not allow _ or - at beginning', function() {
-        expect(idToDomId('_x')).toBe('x');
-        expect(idToDomId('-x')).toBe('x');
-        expect(idToDomId('-__-x')).toBe('x');
-        expect(idToDomId('Foo')).toBe('foo');
-    });
-    it('should not allow _ or - at end', function() {
-        expect(idToDomId('x_')).toBe('x');
-        expect(idToDomId('x-')).toBe('x');
-    });
-    it('should not allow _- or -_', function() {
-        expect(idToDomId('foo_--bar-__baz')).toBe('foo-bar-baz');
     });
 });
