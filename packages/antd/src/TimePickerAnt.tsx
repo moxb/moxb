9ea0badba3f69c3ab07b59c4ead5d5b518dec8e2
Stefan Herndlbauer <sherndlbauer@gorillabyte.com>
2018-09-28 11:46:04 +0200

    Add form components for datePicker and timePicker

--- packages/antd/src/TimePickerAnt.tsx
+++ packages/antd/src/TimePickerAnt.tsx
@@ -1,16 +1,17 @@
+import { FormItemProps } from 'antd/lib/form';
 import { observer } from 'mobx-react';
 import * as React from 'react';
-import { TimePicker } from 'antd';
+import { TimePicker, Form } from 'antd';
 import { TimePickerProps } from 'antd/lib/time-picker';
-import { parseProps } from './BindAnt';
+import { parseProps, getErrorMessages } from './BindAnt';
 import { Time } from '@moxb/moxb';
 
-export interface BindTimePickerAntProps {
+export interface BindTimePickerAntProps extends TimePickerProps {
     operation: Time;
 }
 
 @observer
-export class TimePickerAnt extends React.Component<TimePickerProps & BindTimePickerAntProps> {
+export class TimePickerAnt extends React.Component<BindTimePickerAntProps> {
     render() {
         const { operation, ...props } = parseProps(this.props, this.props.operation);
         return (
@@ -19,6 +20,24 @@ export class TimePickerAnt extends React.Component<TimePickerProps & BindTimePic
                 onChange={(time: any, timeString: string) => operation.setValue(timeString)}
                 {...props as any}
             />
+        );
+    }
+}
+
+@observer
+export class TimePickerFormAnt extends React.Component<BindTimePickerAntProps & FormItemProps> {
+    render() {
+        const { operation, ...props } = parseProps(this.props, this.props.operation);
+        return (
+            <Form.Item
+                required={operation.required}
+                hasFeedback={operation.errors!.length > 0}
+                validateStatus={operation.errors!.length > 0 ? 'error' : undefined}
+                help={operation.errors!.length > 0 ? getErrorMessages(operation.errors!) : null}
+                {...props as any}
+            >
+                <TimePickerAnt operation={this.props.operation} />
+            </Form.Item>
         );
     }
 }
