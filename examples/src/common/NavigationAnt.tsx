2e9b766c09ab4a6535ee952aa1eb4af639b4e851
csillag <csillag@harmacolindor.com>
2018-10-23 23:25:14 +0200

    Follow API change

--- examples/src/common/NavigationAnt.tsx
+++ examples/src/common/NavigationAnt.tsx
@@ -1,12 +1,12 @@
 import { inject, observer } from 'mobx-react';
 import * as React from 'react';
-import { NavMenuBar } from '@moxb/antd';
+import { NavMenuBarAnt } from '@moxb/antd';
 
 import { mainMenu, missingContent } from '../MenuStructure.tsx';
 
 @inject('location')
 export class NavigationAnt extends React.Component<{ location?: LocationManager }> {
     render() {
-        return <NavMenuBar locationManager={this.props.location} substates={mainMenu} />;
+        return <NavMenuBarAnt locationManager={this.props.location} substates={mainMenu} />;
     }
 }
