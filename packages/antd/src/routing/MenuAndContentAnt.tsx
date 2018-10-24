a9f41997a4a8540304f0346f70ac92b49335eb78
csillag <csillag@harmacolindor.com>
2018-10-24 10:39:56 +0200

    Simplify

--- packages/antd/src/routing/MenuAndContentAnt.tsx
+++ packages/antd/src/routing/MenuAndContentAnt.tsx
@@ -7,40 +7,10 @@ export type UIProps = NavMenuProps & ContentProps;
 
 export class MenuAndContentAnt extends React.Component<UIProps, {}> {
     public render() {
-        const {
-            locationManager,
-            arg,
-            parsedTokens,
-            substates,
-            filterCondition,
-            hierarchical,
-            right,
-            fallback,
-            mountAll,
-            debug,
-        } = this.props;
-        const menuProps: NavMenuProps = {
-            locationManager,
-            parsedTokens,
-            arg,
-            substates,
-            filterCondition,
-            hierarchical,
-            right,
-        };
-        const contentProps: ContentProps = {
-            locationManager,
-            parsedTokens,
-            arg,
-            substates,
-            fallback,
-            mountAll,
-            debug,
-        };
         return (
             <div>
-                <NavMenuBarAnt {...menuProps} />
-                <LocationDependentContent {...contentProps} />
+                <NavMenuBarAnt {...this.props} />
+                <LocationDependentContent {...this.props} />
             </div>
         );
     }
