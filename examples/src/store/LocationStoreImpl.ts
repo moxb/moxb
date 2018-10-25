b51c24f765d5e7c43d254ab45d150ac9e35fa5f2
csillag <csillag@harmacolindor.com>
2018-10-25 11:39:39 +0200

    Follow API changes

--- examples/src/store/LocationStoreImpl.ts
+++ examples/src/store/LocationStoreImpl.ts
@@ -1,9 +1,17 @@
-import { LocationManager, PATH_STRATEGY, BasicLocationManagerImpl } from '@moxb/moxb';
+import {
+    LocationManager,
+    NativeUrlSchema,
+    QueryBasedUrlSchema,
+    HashBasedUrlSchema,
+    BasicLocationManagerImpl,
+} from '@moxb/moxb';
 
 export class LocationStoreImpl extends BasicLocationManagerImpl {
     public constructor() {
         super({
-            pathStrategy: PATH_STRATEGY.QUERY,
+            //            urlSchema: new NativeUrlSchema(),
+            urlSchema: new QueryBasedUrlSchema(),
+            //            urlSchema: new HashBasedUrlSchema(),
             cleanSeparatorFromPathEnd: true,
         });
         this.watchHistory();
