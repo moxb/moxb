f821eaf82873ba58e8129b31eaf0738c85b45ee5
Stefan Herndlbauer <sherndlbauer@gorillabyte.com>
2018-12-03 18:38:50 +0100

    Disable moxb/html for now

--- examples/src/app/ApplicationHTML.tsx
+++ examples/src/app/ApplicationHTML.tsx
@@ -1,4 +1,4 @@
-import { ActionButtonHtml, ActionAnchorHtml } from '@moxb/html/dist/ActionHtml';
+// import { ActionButtonHtml, ActionAnchorHtml } from '@moxb/html/dist/ActionHtml';
 import { inject, observer } from 'mobx-react';
 import * as React from 'react';
 import { toJSON } from '@moxb/moxb';
@@ -13,18 +13,18 @@ import { Application } from './Application';
 @observer
 export class ApplicationHTML extends React.Component<{ app?: Application }> {
     render() {
-        const application = this.props.app!;
+        // const application = this.props.app!;
         return (
             <main>
                 <section>
                     <h1>HTML Components</h1>
                     <hr />
                     <h3>ActionButtonUI Component</h3>
-                    <ActionButtonHtml operation={application.testAction} />
+                    {/*<ActionButtonHtml operation={application.testAction} />*/}
                     <br />
                     <br />
                     <h3>ActionFormButtonUI Component</h3>
-                    <ActionAnchorHtml operation={application.testAction} />
+                    {/*<ActionAnchorHtml operation={application.testAction} />*/}
                     <br />
                     <br />
                 </section>
