7a9ef45347ee7e70f3e041b1ae150c6d1f7755d1
Stefan Herndlbauer <sherndlbauer@gorillabyte.com>
2018-12-18 15:10:02 +0100

    Change the unit display to the ant design formatter/parser implementation

--- packages/antd/src/NumericAnt.tsx
+++ packages/antd/src/NumericAnt.tsx
@@ -14,24 +14,26 @@ export class NumericAnt extends React.Component<BindAntProps<Numeric> & InputNum
         if (invisible) {
             return null;
         }
+        const formatter = (value?: number | string) => (operation.unit ? `${value}${operation.unit}` : `${value}`);
+        const parser = (value?: string) =>
+            operation.unit ? parseInt(value!.replace(`${operation.unit}`, '') || '0') : parseInt(value || '0');
         return (
-            <span>
-                <InputNumber
-                    id={id}
-                    step={operation.step}
-                    min={operation.min}
-                    max={operation.max}
-                    value={operation.value}
-                    size={size as any}
-                    placeholder={operation.placeholder || 'Interval'}
-                    onChange={value => {
-                        operation.setValue(parseInt(value as string));
-                        operation.onExitField();
-                    }}
-                    {...props}
-                />
-                {operation.unit && ' ' + operation.unit}
-            </span>
+            <InputNumber
+                id={id}
+                step={operation.step}
+                min={operation.min}
+                max={operation.max}
+                value={operation.value}
+                size={size as any}
+                placeholder={operation.placeholder || 'Interval'}
+                formatter={formatter}
+                parser={parser}
+                onChange={value => {
+                    operation.setValue(parseInt((value as string) || '0'));
+                    operation.onExitField();
+                }}
+                {...props}
+            />
         );
     }
 }
