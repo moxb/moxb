c2f6ed9bac9ca603c9613acf8b6919185ac0651c
csillag <csillag@harmacolindor.com>
2019-05-14 19:36:01 +0200

    Introduce the NavRadioButtonBarAnt component

--- packages/antd/src/routing/index.ts
+++ packages/antd/src/routing/index.ts
@@ -4,3 +4,4 @@ export { NavTabBarAnt } from './NavTabBarAnt';
 export { MenuAndContentAnt } from './MenuAndContentAnt';
 export { NavStepsAnt } from './NavStepsAnt';
 export { MultiStepProcessAnt } from './MultiStepProcessAnt';
+export { NavRadioButtonBarAnt } from './NavRadioButtonsBarAnt';
