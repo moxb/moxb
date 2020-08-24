ac9609b46de9e862779ffaa98ad1b3f2fed95766
csillag <csillag@harmacolindor.com>
2020-08-24 15:33:06 +0200

    Add some debug output to form submission

--- packages/moxb/src/form/FormImpl.ts
+++ packages/moxb/src/form/FormImpl.ts
@@ -52,20 +52,20 @@ export class FormImpl extends BindImpl<FormOptions> implements Form {
 
     @computed
     get canSubmitForm() {
-        return this.values.findIndex(v => !!v.isInitialValue) < 0;
+        return this.values.findIndex((v) => !!v.isInitialValue) < 0;
     }
 
     @computed
     get allErrors(): string[] {
-        const valuesWithErrors = this.values.filter(v => !!v.errors);
+        const valuesWithErrors = this.values.filter((v) => !!v.errors);
         // Set keeps the items in insertion order
         const allErrors = new Set<string>();
-        valuesWithErrors.forEach(v => {
-            v.errors!.forEach(error => {
+        valuesWithErrors.forEach((v) => {
+            v.errors!.forEach((error) => {
                 allErrors.add(v.label ? v.label + ': ' + t(error, error) : t(error, error));
             });
         });
-        this.errors!.forEach(error => {
+        this.errors!.forEach((error) => {
             allErrors.add(this.label ? this.label + ': ' + t(error, error) : t(error, error));
         });
         return Array.from(allErrors);
@@ -73,22 +73,23 @@ export class FormImpl extends BindImpl<FormOptions> implements Form {
 
     @computed
     get hasErrors() {
-        return !!(this.values.find(v => v.errors!.length > 0) || this.errors!.length > 0);
+        return !!(this.values.find((v) => v.errors!.length > 0) || this.errors!.length > 0);
     }
 
     @computed
     get hasChanges() {
-        return !this.values.every(v => (v.isInitialValue === undefined ? true : v.isInitialValue));
+        return !this.values.every((v) => (v.isInitialValue === undefined ? true : v.isInitialValue));
     }
 
     @computed
     get hasMissingRequired() {
-        return !this.values.filter(v => v.required).every(v => v.isGiven);
+        return !this.values.filter((v) => v.required).every((v) => v.isGiven);
     }
 
     @action.bound
     onSubmitForm() {
-        this.values.forEach(v => v.save());
+        console.log('FormImpl.onSubmitForm()');
+        this.values.forEach((v) => v.save());
         if (this.impl.onSubmit) {
             this.impl.onSubmit(this as any, this.submitDone);
         }
@@ -105,13 +106,13 @@ export class FormImpl extends BindImpl<FormOptions> implements Form {
 
     @action.bound
     resetValues() {
-        this.values.forEach(v => v.resetToInitialValue());
+        this.values.forEach((v) => v.resetToInitialValue());
     }
 
     @action.bound
     clearAllErrors() {
         if (this.values) {
-            this.values.forEach(v => v.clearErrors());
+            this.values.forEach((v) => v.clearErrors());
             this.clearErrors();
         }
     }
