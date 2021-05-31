dd09cc19fc268a95fe46a0e6dda5086e8d2164dd
csillag <csillag@harmacolindor.com>
2021-05-31 17:41:34 +0200

    Add support for saving previous path when executing redirects

--- packages/html/src/Redirect.tsx
+++ packages/html/src/Redirect.tsx
@@ -1,23 +1,53 @@
-import { UpdateMethod, UsesLocation, NavRef } from '@moxb/moxb';
+import { UpdateMethod, UsesLocation, NavRef, UrlArg, MyLocation } from '@moxb/moxb';
 import { inject, observer } from 'mobx-react';
 import * as React from 'react';
 
 interface RedirectProps {
     position?: number;
-    to: string[];
+
+    /**
+     * Where should be go? (List of path tokens.)
+     *
+     * Either this or location should be given.
+     */
+    to?: string[];
+
     updateMethod?: UpdateMethod;
+
+    /**
+     * If you want the original path to be saved, provide an UrlArg for that.
+     * (Note that the arg should be permanent, in order to survive the path change.)
+     */
+    pathSaveArg?: UrlArg<MyLocation | null>;
+
+    /**
+     * Are we trying to restore a saved location? Then provide that location
+     *
+     * When supplied, this will override all other values.
+     */
+    location?: MyLocation;
 }
 
 @inject('locationManager')
 @observer
 export class Redirect extends React.Component<UsesLocation & RedirectProps> {
     public componentDidMount() {
-        const { locationManager, position = 0, to, updateMethod } = this.props;
-        locationManager!.doSetPathTokens(position, to, updateMethod);
+        const { locationManager, position = 0, to, updateMethod, pathSaveArg, location } = this.props;
+        if (location) {
+            locationManager!.doSetLocation(location, UpdateMethod.REPLACE);
+            return;
+        }
+        if (pathSaveArg) {
+            pathSaveArg.doSet(locationManager!.location);
+        }
+        if (to !== undefined) {
+            // An empty list is a valid input here, so we can't simply test for falsy
+            locationManager!.doSetPathTokens(position, to, updateMethod);
+        }
     }
 
     render() {
-        return <div>Redirecting to {this.props.to.join('/')} ... </div>;
+        return <div>Redirecting ... </div>;
     }
 }
 
