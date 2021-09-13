fd85497ac47e66b2166371c0a3d4fb38f3f4060b
Gergely Ujvari <ujvari@nolmecolindor.com>
2021-09-13 16:30:54 +0200

    Remove React-dropzone based file uploaeder

--- packages/antd/src/FileUploadAnt.tsx
+++ packages/antd/src/FileUploadAnt.tsx
@@ -1,7 +1,6 @@
 const path = require('path');
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import Dropzone from 'react-dropzone';
 import { Spin } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
 
@@ -13,93 +12,6 @@ import Dragger, { DraggerProps } from 'antd/es/upload/Dragger';
 
 export interface FileUploadProps {
     operation: FileUpload;
-}
-
-@observer
-export class FileUploadAnt extends React.Component<FileUploadProps> {
-    componentDidMount() {
-        this.props.operation.reset();
-    }
-
-    onDrop(files: File[]) {
-        if (files.length) {
-            this.props.operation.upload(files[0]);
-        }
-    }
-
-    constructor(props: FileUploadProps) {
-        super(props);
-        this.onDrop = this.onDrop.bind(this);
-    }
-
-    render() {
-        const {
-            succeeded,
-            successLabel,
-            errorLabel,
-            progressLabel,
-            resetAction,
-            pending,
-            prompt,
-            promptTitle,
-            visible,
-        } = this.props.operation;
-
-        return (
-            visible && (
-                <div>
-                    <LabelAnt operation={progressLabel} />
-                    {pending && <Spin />}
-                    <LabelAnt
-                        operation={errorLabel}
-                        style={{
-                            color: 'red',
-                            fontWeight: 'bold',
-                        }}
-                    />
-                    <LabelAnt
-                        operation={successLabel}
-                        style={{
-                            color: 'green',
-                            fontWeight: 'bold',
-                        }}
-                    />
-                    {!(succeeded || pending) && (
-                        <Dropzone onDrop={this.onDrop} maxFiles={1}>
-                            {({ getRootProps, getInputProps }) => (
-                                <section>
-                                    <div {...getRootProps()}>
-                                        <input {...getInputProps()} />
-                                        {/*Using classes from https://ant.design/components/upload/ */}
-                                        <div className="ant-upload ant-upload-drag">
-                                            <p className="ant-upload-drag-icon">
-                                                <InboxOutlined />
-                                            </p>
-                                            {promptTitle && promptTitle.length && (
-                                                <p className="ant-upload-text">{promptTitle}</p>
-                                            )}
-                                            {prompt}
-                                        </div>
-                                    </div>
-                                </section>
-                            )}
-                        </Dropzone>
-                    )}
-                    <ActionButtonAnt operation={resetAction} />
-                </div>
-            )
-        );
-    }
-}
-
-export class FileUploadFormAnt extends React.Component<BindFormItemAntProps & { operation: FileUpload }> {
-    render() {
-        return (
-            <FormItemAnt {...this.props}>
-                <FileUploadAnt operation={this.props.operation} />
-            </FormItemAnt>
-        );
-    }
 }
 
 @observer
