7c3b0f4a437b5eb2da95b1ff273ab2b154624db6
Csillag Kristof <csillag@harmacolindor.com>
2022-06-28 14:32:11 +0200

    Fix code formatting

--- packages/meteor/src/MeteorPublication.ts
+++ packages/meteor/src/MeteorPublication.ts
@@ -117,8 +117,18 @@ export interface RegisterMeteorPublicationProps<Input, Output> {
 export function registerMeteorPublication<Input, Output>(
     params: RegisterMeteorPublicationProps<Input, Output>
 ): MeteorPublicationHandle<Input, Output> {
-    const { name, collection, prePublish, selector, clientSelector, skipIf, auth, options, clientOptions, debugMode } =
-        params;
+    const {
+        name,
+        collection,
+        prePublish,
+        selector,
+        clientSelector,
+        skipIf,
+        auth,
+        options,
+        clientOptions,
+        debugMode,
+    } = params;
     const logger = getDebugLogger(`publication ${name}`, debugMode);
 
     const getServerCursor = (args: Input, sub: Subscription) => {
