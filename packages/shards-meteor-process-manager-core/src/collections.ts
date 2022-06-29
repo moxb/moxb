3a7a7a5f35b35e8e23a6a96f4e433c6b8c4578c1
Csillag Kristof <csillag@nolmecolindor.com>
2022-06-29 20:42:04 +0200

    Keep trying to make Meteor imports work correctly in NPM packages

--- packages/shards-meteor-process-manager-core/src/collections.ts
+++ packages/shards-meteor-process-manager-core/src/collections.ts
@@ -1,4 +1,6 @@
-import { Mongo } from 'meteor/mongo';
+import type { Mongo as CorrectMongo } from 'meteor/mongo';
 import { ProcessRecord } from './types';
 
-export const ProcessCollection = new Mongo.Collection<ProcessRecord>('background-process-manager---processes');
+export const ProcessCollection = new Mongo.Collection<ProcessRecord>(
+    'background-process-manager---processes'
+) as CorrectMongo.Collection<ProcessRecord, ProcessRecord>;
