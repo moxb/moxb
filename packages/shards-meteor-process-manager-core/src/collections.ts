acf6d74c0d0acbe62af2f0514806b7fd855ee740
Csillag Kristof <csillag@nolmecolindor.com>
2022-06-29 21:37:01 +0200

    Remove debug code

--- packages/shards-meteor-process-manager-core/src/collections.ts
+++ packages/shards-meteor-process-manager-core/src/collections.ts
@@ -1,8 +1,6 @@
 import type { Mongo as CorrectMongo } from 'meteor/mongo';
 import { ProcessRecord } from './types';
 
-console.log('Latest code');
-
 export const ProcessCollection = new Mongo.Collection<ProcessRecord>(
     'background-process-manager---processes'
 ) as CorrectMongo.Collection<ProcessRecord, ProcessRecord>;
