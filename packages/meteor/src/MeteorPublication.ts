3c66ac53d580c97f8dc8a98b635313d99834d490
Csillag Kristof <csillag@nolmecolindor.com>
2022-06-30 02:31:35 +0200

    Tighten an import to avoid unnecessary deps

--- packages/meteor/src/MeteorPublication.ts
+++ packages/meteor/src/MeteorPublication.ts
@@ -1,4 +1,4 @@
-import { getDebugLogger } from '@moxb/moxb';
+import { getDebugLogger } from '@moxb/moxb/dist/util/debugLog';
 import type { Mongo } from 'meteor/mongo';
 
 export interface MeteorPublicationHandle<Input, Output> {
@@ -117,18 +117,8 @@ export interface RegisterMeteorPublicationProps<Input, Output> {
 export function registerMeteorPublication<Input, Output>(
     params: RegisterMeteorPublicationProps<Input, Output>
 ): MeteorPublicationHandle<Input, Output> {
-    const {
-        name,
-        collection,
-        prePublish,
-        selector,
-        clientSelector,
-        skipIf,
-        auth,
-        options,
-        clientOptions,
-        debugMode,
-    } = params;
+    const { name, collection, prePublish, selector, clientSelector, skipIf, auth, options, clientOptions, debugMode } =
+        params;
     const logger = getDebugLogger(`publication ${name}`, debugMode);
 
     const getServerCursor = (args: Input, sub: Subscription) => {
