99059457c17c9b8563b46276dbd2948e19e71805
Csillag Kristof <csillag@harmacolindor.com>
2022-07-02 17:42:05 +0200

    Format code

--- packages/shards-accounts-flow-antd/src/forms/LoginForm.tsx
+++ packages/shards-accounts-flow-antd/src/forms/LoginForm.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import {LoginFormUI, LoginFormUIProps} from '@moxb/shards-accounts-ui-antd';
-import {getLinks} from '../links';
-import {useAuthBackend} from '../authContext';
+import { LoginFormUI, LoginFormUIProps } from '@moxb/shards-accounts-ui-antd';
+import { getLinks } from '../links';
+import { useAuthBackend } from '../authContext';
 
 type LoginFormProps = Pick<LoginFormUIProps, 'splash'>;
 
 export function LoginForm(props: LoginFormProps) {
     const backend = useAuthBackend()!;
-    const {loginErrorMessage, isLoginPending} = backend.useLoginStatus();
+    const { loginErrorMessage, isLoginPending } = backend.useLoginStatus();
     const links = getLinks();
 
     return (
@@ -15,7 +15,7 @@ export function LoginForm(props: LoginFormProps) {
             splash={props.splash}
             error={loginErrorMessage}
             loginPending={isLoginPending}
-            onLogin={({username, password}) => backend.triggerLogin(username, password)}
+            onLogin={({ username, password }) => backend.triggerLogin(username, password)}
             registerLink={links.register}
             forgotPasswordLink={links.forgotPassword}
         />
--- packages/shards-accounts-flow-antd/src/forms/PasswordResetForm.tsx
+++ packages/shards-accounts-flow-antd/src/forms/PasswordResetForm.tsx
@@ -1,27 +1,27 @@
 import * as React from 'react';
-import {observer} from 'mobx-react-lite';
-import {useLocationManager} from '@moxb/stellar-router-react';
-import {PasswordResetFormUI, PasswordResetFormUIProps} from '@moxb/shards-accounts-ui-antd';
+import { observer } from 'mobx-react-lite';
+import { useLocationManager } from '@moxb/stellar-router-react';
+import { PasswordResetFormUI, PasswordResetFormUIProps } from '@moxb/shards-accounts-ui-antd';
 
-import {getLinks} from '../links';
-import {useAuthBackend} from '../authContext';
+import { getLinks } from '../links';
+import { useAuthBackend } from '../authContext';
 
 type PasswordResetFormProps = Pick<PasswordResetFormUIProps, 'splash'>;
 
 export const PasswordResetForm = observer((props: PasswordResetFormProps) => {
-    const {splash} = props;
+    const { splash } = props;
     const backend = useAuthBackend()!;
 
     const locationManager = useLocationManager('password reset form');
     const token = locationManager?._query.token;
-    const {resetErrorMessage, isResetPending} = backend.useResetStatus();
+    const { resetErrorMessage, isResetPending } = backend.useResetStatus();
     const links = getLinks();
     return (
         <PasswordResetFormUI
             splash={splash}
             resetPending={isResetPending}
             error={resetErrorMessage}
-            onReset={({password1, password2}) => backend.triggerPasswordReset(token, password1, password2)}
+            onReset={({ password1, password2 }) => backend.triggerPasswordReset(token, password1, password2)}
             loginLink={links.login}
         />
     );
