2cd5197b79f781a29d4b633e07c466f416a8c5a2
Csillag Kristof <csillag@harmacolindor.com>
2022-07-04 14:35:37 +0200

    Fix a hook issue

--- packages/shards-accounts-flow-antd/src/WithLoginFlow.tsx
+++ packages/shards-accounts-flow-antd/src/WithLoginFlow.tsx
@@ -52,30 +52,28 @@ export function WithLoginFlow(props: WithLoginFlowProps) {
 
     const logic: AuthenticationLogic = new AuthenticationLogicImpl(backend);
 
-    const isLoggedIn = logic.isLoggedIn;
-
     const loginMenu: StateSpace<string, UIFragment, void> = {
         metaData: 'login menu',
         subStates: [
             {
                 key: PATH.login,
-                fragment: () => (isLoggedIn ? redirectToApp() : <LoginForm splash={splash} />),
+                fragment: () => (logic.isLoggedIn ? redirectToApp() : <LoginForm splash={splash} />),
             },
             {
                 key: PATH.register,
-                fragment: () => (isLoggedIn ? redirectToApp() : <RegistrationForm splash={splash} />),
+                fragment: () => (logic.isLoggedIn ? redirectToApp() : <RegistrationForm splash={splash} />),
             },
             {
                 key: PATH.forgotPassword,
-                fragment: () => (isLoggedIn ? redirectToApp() : <ForgotPasswordForm splash={splash} />),
+                fragment: () => (logic.isLoggedIn ? redirectToApp() : <ForgotPasswordForm splash={splash} />),
             },
             {
                 key: PATH.verifyEmail,
-                fragment: () => (isLoggedIn ? redirectToApp() : <span>Should verify email</span>),
+                fragment: () => (logic.isLoggedIn ? redirectToApp() : <span>Should verify email</span>),
             },
             {
                 key: PATH.resetPassword,
-                fragment: () => (isLoggedIn ? redirectToApp() : <PasswordResetForm splash={splash} />),
+                fragment: () => (logic.isLoggedIn ? redirectToApp() : <PasswordResetForm splash={splash} />),
             },
         ],
         fallback: () => children,
