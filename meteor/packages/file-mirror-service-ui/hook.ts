bc93d05a9bc90eed6bf7a8c60418e585bbfaeae6
Csillag Kristof <csillag@harmacolindor.com>
2022-07-06 20:06:40 +0200

    Follow dependencies

--- meteor/packages/file-mirror-service-ui/hook.ts
+++ meteor/packages/file-mirror-service-ui/hook.ts
@@ -1,12 +1,12 @@
-import { useMeteorMethodResult } from '@moxb/meteor-react';
-import { methodGetMirroredFileInfo } from 'meteor/moxb:file-mirror-service';
-import type { MirroredFileInfo } from 'meteor/moxb:file-mirror-service';
+import {useRPCResult} from '@moxb/react-html';
+import {methodGetMirroredFileInfo} from 'meteor/moxb:file-mirror-service';
+import type {MirroredFileInfo} from 'meteor/moxb:file-mirror-service';
 
 export function useMirroredFile(originalUrl: string): [
     boolean, // pending
-    string | undefined, // error
-    MirroredFileInfo | undefined, // result
+        string | undefined, // error
+        MirroredFileInfo | undefined, // result
     () => void // trigger
 ] {
-    return useMeteorMethodResult(methodGetMirroredFileInfo, originalUrl, { stable: true, swallowErrors: true });
+    return useRPCResult(methodGetMirroredFileInfo, originalUrl, {stable: true, swallowErrors: true});
 }
--- meteor/packages/file-mirror-service-ui/package.js
+++ meteor/packages/file-mirror-service-ui/package.js
@@ -1,21 +1,20 @@
 Package.describe({
-  name: 'moxb:file-mirror-service-ui',
-  version: '0.0.3',
-  summary: 'Client-side widgets for your file mirror service',
-  git: 'https://github.com/moxb/moxb/tree/master/meteor/packages/file-mirror-service-ui',
-  documentation: 'README.md'
+    name: 'moxb:file-mirror-service-ui',
+    version: '0.0.5',
+    summary: 'Client-side widgets for your file mirror service',
+    git: 'https://github.com/moxb/moxb/tree/master/meteor/packages/file-mirror-service-ui',
+    documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('2.7.3');
-  api.use('ecmascript');
-  api.use('typescript');
-  api.use('moxb:file-mirror-service@0.0.5');
-  api.mainModule('index.ts', "client");
+Package.onUse(function (api) {
+    api.versionsFrom('2.7.3');
+    api.use('ecmascript');
+    api.use('typescript');
+    api.use('moxb:file-mirror-service@0.0.11');
+    api.mainModule('index.ts', "client");
 });
 
 Npm.depends({
-  "@moxb/moxb": '0.3.28',
-  "@moxb/meteor-react": '0.3.28',
-  "@moxb/react-html": '0.3.28'
+    "@moxb/moxb": '0.3.28',
+    "@moxb/react-html": '0.3.62'
 });
