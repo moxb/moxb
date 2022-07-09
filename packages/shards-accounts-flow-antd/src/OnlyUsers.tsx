f22fb6d766939e30f73255abfb63628be45107f9
Csillag Kristof <csillag@harmacolindor.com>
2022-07-09 04:08:27 +0200

    shards-accounts-flow-antd: better debugging

--- packages/shards-accounts-flow-antd/src/OnlyUsers.tsx
+++ packages/shards-accounts-flow-antd/src/OnlyUsers.tsx
@@ -1,15 +1,18 @@
 import { observer } from 'mobx-react-lite';
 
+import { getDebugLogger } from '@moxb/moxb';
 import { MyLocation, redirect, UpdateMethod, useLocationManager } from '@moxb/stellar-router-react';
 
 import { useAuthenticationLogic } from './authContext';
 
 interface OnlyUsersProps {
     children: JSX.Element;
+    debug?: boolean;
 }
 
 export const OnlyUsers = observer((props: OnlyUsersProps): JSX.Element | null => {
-    const { children } = props;
+    const { children, debug } = props;
+    const logger = getDebugLogger('OnlyUsers', debug);
 
     const auth = useAuthenticationLogic();
 
@@ -18,15 +21,18 @@ export const OnlyUsers = observer((props: OnlyUsersProps): JSX.Element | null =>
 
     // If the login state is not yet known, we will just wait
     if (!auth.isLoginStatusKnown) {
+        logger.log('Auth status unknown, waiting');
         return null;
     }
 
     // If we are logged in, then we can simply display the normal content.
     if (auth.isLoggedIn) {
+        logger.log('Logged in, nothing to do');
         return children;
     }
 
     // It seems like we are supposed to be logged in, but we aren't. We will have to redirect...
+    logger.log('Not logged in, redirecting...');
     return redirect({
         to: ['login'],
         updateMethod: UpdateMethod.REPLACE,
