920fcb33e07288554fa25629700df66a41a4ca88
Csillag Kristof <csillag@harmacolindor.com>
2022-07-11 09:16:11 +0200

    Action: auto-track pending status and errors

--- packages/moxb/src/action/Action.ts
+++ packages/moxb/src/action/Action.ts
@@ -3,14 +3,31 @@ import { Bind } from '../bind/Bind';
 export interface Action extends Bind {
     /**
      * A confirmation question that should be put to the user before really firing the action
+     *
+     * (Please note that this behaviour must be implemented by the UI element exposing the Action,
+     * and not by ActionImpl itself.)
      */
     readonly confirmQuestion?: string;
 
+    /**
+     * Execute the action.
+     *
+     * Call this version if you are not interested in any results.
+     */
     fire(): void;
 
+    /**
+     * Execute the action, and return a promise.
+     *
+     * Call this version if you want to observe the results.
+     */
     firePromise(): Promise<void>;
 
+    /**
+     * Is the action currently being executed?
+     */
     readonly pending: boolean;
+
     /**
      * Keyboard shortcuts associated with this action. Interpretation depends on the implementation in the
      * {@Keyboard}
--- packages/moxb/src/action/ActionImpl.ts
+++ packages/moxb/src/action/ActionImpl.ts
@@ -1,4 +1,4 @@
-import { action, computed, makeObservable } from 'mobx';
+import { action, computed, makeObservable, observable } from 'mobx';
 import { BindImpl, BindOptions, getValueOrFunction, ValueOrFunction } from '../bind/BindImpl';
 import { Action } from './Action';
 
@@ -22,7 +22,7 @@ export class ActionImpl extends BindImpl<ActionOptions> implements Action {
     }
 
     protected getPending() {
-        return this.impl.pending ? this.impl.pending() : false;
+        return (this.impl.pending ? this.impl.pending() : false) || this._promisePending;
     }
 
     get pending() {
@@ -40,28 +40,49 @@ export class ActionImpl extends BindImpl<ActionOptions> implements Action {
         return this.impl.keyboardShortcuts;
     }
 
+    @observable
+    _promisePending = false;
+
     @action.bound
     firePromise() {
-        return new Promise<void>((resolve, reject) => {
-            if (this.enabled) {
-                if (this.pending) {
-                    const problem = `cannot fire pending action ${this.id} '${this.label}'`;
-                    console.warn(problem);
-                    reject(problem);
-                } else {
+        if (!this.enabled) {
+            const problem = `cannot fire disabled action ${this.id} '${this.label}': ${this.reason}`;
+            console.warn(problem);
+            return Promise.reject(problem);
+        }
+        if (this.pending) {
+            const problem = `cannot fire pending action ${this.id} '${this.label}'`;
+            console.warn(problem);
+            return Promise.reject(problem);
+        }
+        this.clearErrors();
+        const promise = new Promise<void>(
+            action((resolve, reject) => {
+                this._promisePending = true;
+                try {
                     const firing = this.impl.fire();
                     if (firing) {
+                        // The execution has returned a promise, so we can use that to track process.
                         firing.then(resolve, reject);
                     } else {
+                        // No promise was returned, so we will assume that a successful execution mean that we are done.
                         resolve();
                     }
+                } catch (error) {
+                    // We couldn't even fire this, so it's an instant fail
+                    reject(error);
                 }
-            } else {
-                const problem = `cannot fire disabled action ${this.id} '${this.label}': ${this.reason}`;
-                console.warn(problem);
-                reject(problem);
-            }
+            })
+        );
+        promise.catch((error) => {
+            this.setError(error.reason || error.error || error.toString());
         });
+        promise.finally(
+            action(() => {
+                this._promisePending = false;
+            })
+        );
+        return promise;
     }
 
     @action.bound
