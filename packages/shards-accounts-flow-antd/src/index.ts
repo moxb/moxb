8262744eaed023ac47497f89fb41132df9f072ed
Csillag Kristof <csillag@harmacolindor.com>
2022-07-11 11:45:57 +0200

    Add forgotten export

--- packages/shards-accounts-flow-antd/src/index.ts
+++ packages/shards-accounts-flow-antd/src/index.ts
@@ -3,3 +3,4 @@ export { WithLoginFlow } from './WithLoginFlow';
 export { LogoutButton } from './LogoutButton';
 export { LOGIN_SYSTEM_PATH } from './paths';
 export { AuthenticationBackend } from './AuthenticationBackend';
+export { useAuthenticationLogic } from './authContext';
