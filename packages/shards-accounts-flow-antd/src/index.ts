1a0f6f31b2d6f758620176a541cb0dcaf24d94e0
Csillag Kristof <csillag@nolmecolindor.com>
2022-07-11 12:31:13 +0200

    Add forgotten export

--- packages/shards-accounts-flow-antd/src/index.ts
+++ packages/shards-accounts-flow-antd/src/index.ts
@@ -4,3 +4,4 @@ export { LogoutButton } from './LogoutButton';
 export { LOGIN_SYSTEM_PATH } from './paths';
 export { AuthenticationBackend } from './AuthenticationBackend';
 export { useAuthenticationLogic } from './authContext';
+export { SimpleLoginApp } from './SimpleLoginApp';
