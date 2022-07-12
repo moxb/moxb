ad70472c174254847b128be8d1095b5983893612
Csillag Kristof <csillag@nolmecolindor.com>
2022-07-12 04:24:16 +0200

    Improve HighlightedText to better handle normalization

--- packages/react-html/src/highlighter/highlighter.ts
+++ packages/react-html/src/highlighter/highlighter.ts
@@ -1,15 +1,16 @@
 import * as React from 'react';
+
+import { findTextMatch } from '@moxb/moxb';
+import type { NormalizerOptions } from '@moxb/moxb';
+
 import { styleToString } from '../util';
 
-/**
- * Store info about where did we found the pattern inside the corpus
- */
-interface MatchInfo {
-    searchText: string;
-    startPos: number;
-}
-
 export interface HighlightOptions {
+    /**
+     * Options for identifying the matches
+     */
+    findOptions?: NormalizerOptions;
+
     /**
      * Which class to use for highlighting?
      *
@@ -26,28 +27,6 @@ export interface HighlightOptions {
 }
 
 /**
- * Identify pattern matches within a corpus
- */
-const findMatch = (inValue: string | null | undefined, search: (string | undefined)[]): MatchInfo => {
-    let matchStart: number;
-    const value = (inValue || '').toLowerCase();
-    const matches: MatchInfo[] = search
-        .filter((s) => !!s && s.toLowerCase)
-        .map((s) => {
-            matchStart = value.indexOf(s!.toLowerCase());
-            return matchStart !== -1
-                ? {
-                      searchText: s,
-                      startPos: matchStart,
-                  }
-                : undefined;
-        })
-        .filter((m) => !!m)
-        .map((m) => m as MatchInfo); // This last line is only here to make TS happy
-    return matches[0];
-};
-
-/**
  * Highlight search results within a string using HTML spans
  */
 export const highlightSearchResult = (
@@ -55,12 +34,15 @@ export const highlightSearchResult = (
     searchText: string | undefined,
     options: HighlightOptions
 ): string => {
-    const match = findMatch(value, [searchText]);
+    const match = findTextMatch(value, [searchText]);
     const { className, style } = options;
     const begin = className ? `<span class="${className}" >` : `<span style="${styleToString(style!)}">`;
     const end = '</span>';
     if (match) {
-        return value.replace(RegExp(match.searchText, 'i'), (text) => `${begin}${text}${end}`);
+        return `${value.substring(0, match.startPos)}${begin}${value.substring(
+            match.startPos,
+            match.startPos + match.searchText.length
+        )}${end}${value.substring(match.startPos + match.searchText.length)}`;
     } else {
         return value;
     }
