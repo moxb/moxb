664fa206cb70a34aabf6f1124e711d30ce1fa420
Csillag Kristof <csillag@nolmecolindor.com>
2022-07-12 04:21:07 +0200

    moxb: add text search, normalization and regexp stuff

--- packages/moxb/src/index.ts
+++ packages/moxb/src/index.ts
@@ -3,4 +3,5 @@ export * from './types';
 export * from './decision';
 export * from './util/debugLog';
 export * from './util/isAnyOf';
+export * from './util/text-matching';
 export * from './cached-evaluator';
