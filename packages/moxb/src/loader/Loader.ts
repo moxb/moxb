6464ef3b4d5b1ff9b4eb3f616c913ffcab926230
Kristof Csillag <csillag@nolmecolindor.com>
2022-11-30 14:31:17 +0100

    Loader: add lots of features
    Stable mode, cacheTime, polling, relax(), etc.

--- packages/moxb/src/loader/Loader.ts
+++ packages/moxb/src/loader/Loader.ts
@@ -40,12 +40,29 @@ export interface Loader<Input, Output> {
      * So when everything is ready (most likely, in the constructor of your class using the loader),
      * you should call this method to wake up the loader and load the initial data.
      */
-    awaken(): void;
+    init(): void;
 
     /**
-     * An action to (re)load the data
+     * Tell this loader to "relax", i.e. don't load anything until the output data is requested again.
+     *
+     * My default, the loader will be driven by data: if the input changes, it will reload data,
+     * even is nobody is interested in the output.
+     *
+     * When switched to "relaxed" mode, it will only load data when it is requested by reading the output.
+     * This will remove it from relaxed mode, and it will start to listen,
+     * and it will listen until you tell it to `relax()` again.
+     * The idea is that you could use "relax" when you leave the page when this data is relevant,
+     * so that you don't waste bandwidth and CPU for keeping unwanted data up to date when common input changes.
+     *
+     * Please note that "waking up" from the relaxed state is automatically triggered by some UI code
+     * checking the output of the loader.
      */
-    readonly fetch: Action;
+    relax(): void;
+
+    /**
+     * An action to refresh the data.
+     */
+    readonly refresh: Action;
 
     /**
      * Insert data
@@ -86,9 +103,61 @@ export interface LoaderOptions<Input, Output> {
     load: (input: Input) => Promise<Output>;
 
     /**
+     * Should this loader start in relaxed mode?
+     *
+     * See the `Loader.relax()` method for details.
+     */
+    initRelaxed?: boolean;
+
+    /**
+     * Is this a stable call?
+     *
+     * That is, can we assume that it always returns the same output
+     * for the same input? If yes, we are not going to re-run it, unless the input changes,
+     * or we are explicitly directed to do so.
+     */
+    stable?: boolean;
+
+    /**
+     * If we are loading volatile data (i.e. the same input can result in different output),
+     * what is the minimum amount of time we should wait before loading a new value as requested by the UI?
+     *
+     * (The default is 10 seconds.)
+     *
+     * Please note that this, in itself, doesn't turn on polling; the purpose of this behaviour is simply
+     * to avoid loading the same data multiple times just because the UI is requesting it.
+     *
+     * Please also note that this value is ignored for stable data (see above).
+     */
+    cacheTime?: number;
+
+    /**
+     *  Polling period, in seconds
+     *
+     *  If set, then we will periodically re-load the data.
+     */
+    polling?: number;
+
+    /**
      * Any extra function to call after successful loading
      */
     onLoad?: (output: Output) => void;
+
+    /**
+     * Should we keep previous data while loading new data?
+     *
+     * The default is false, which means that while loading data,
+     * value will temporarily switch to undefined.
+     */
+    keepPreviousData?: boolean;
+
+    /**
+     * Should we keep previous data when (based on the input) we are skipping the loading?
+     *
+     * The default is false, which means then if the loading is skipped,
+     * value will be undefined.
+     */
+    keepDataWhenSkipping?: boolean;
 
     /**
      * Should we run in debug mode?
--- packages/moxb/src/loader/LoaderImpl.ts
+++ packages/moxb/src/loader/LoaderImpl.ts
@@ -2,6 +2,8 @@ import { action, computed, makeObservable, observable, reaction } from 'mobx';
 import { Loader, LoaderOptions } from './Loader';
 import { getDebugLogger, Logger } from '../util/debugLog';
 import { ActionImpl } from '../action/ActionImpl';
+import { isUndefined } from 'lodash';
+import { Action } from '../action/Action';
 
 /**
  * A `Loader` can follow the changes in some input data, and prepare some output data accordingly.
@@ -23,44 +25,170 @@ export class LoaderImpl<Input, Output> implements Loader<Input, Output> {
     }
 
     /**
-     * Load data
+     * The last input value that we have seen
+     *
+     * ... and fetched data for, or decide to skip
      */
-    readonly fetch = new ActionImpl({
-        id: 'loaderAction',
-        fire: (): Promise<any> | undefined => {
-            this._logger.log('Load fired. Input is:', this.input);
-            this.setValue(undefined);
-            if (this.input !== undefined && !this._shouldSkip) {
-                this._logger.log('Will load');
-                const loadPromise = this._options.load(this.input);
-                loadPromise.then(
-                    (result) => {
-                        this._logger.log('Loaded', result);
-                        this.setValue(result);
-                        const { onLoad } = this._options;
-                        if (onLoad) {
-                            onLoad(result);
-                        }
-                    },
-                    (error) => {
-                        this._logger.log('Failed to load:', error);
-                    }
-                );
-                return loadPromise;
-            } else {
-                this._logger.log('Will NOT load');
+    private _fetchedForInput?: Input;
+    private _checkedAt?: number;
+
+    private get __hasInputChanged(): boolean {
+        return JSON.stringify(this.input) !== JSON.stringify(this._fetchedForInput);
+    }
+
+    private get _cacheTime() {
+        return (this._options.cacheTime || 10) * 1000;
+    }
+
+    private get _areWeUpToDate(): boolean {
+        if (isUndefined(this._checkedAt)) {
+            return false;
+        }
+        if (this.__hasInputChanged) {
+            return false;
+        }
+        if (this._options.stable) {
+            return true;
+        } else {
+            const age = Date.now() - this._checkedAt;
+            return age <= this._cacheTime;
+        }
+    }
+
+    private _relaxing = false;
+
+    /**
+     * Relax, stop following changes
+     */
+    relax() {
+        this._logger.log('Relaxing.');
+        this._relaxing = true;
+    }
+
+    private _tick() {
+        delete this._timer;
+        this.refresh.fire();
+    }
+
+    private _clearSchedule() {
+        if (this._timer) {
+            clearTimeout(this._timer);
+            delete this._timer;
+        }
+    }
+
+    private _scheduleNext() {
+        if (this._options.polling) {
+            this._clearSchedule();
+            this._timer = setTimeout(() => this._tick(), this._options.polling * 1000);
+        }
+    }
+
+    /**
+     * Internal loader wrapping the loading
+     * @private
+     */
+    private readonly _load: Action = new ActionImpl({
+        id: 'internalLoader',
+        fire: () => {
+            const input = this.input;
+            this._fetchedForInput = input;
+            this._checkedAt = Date.now();
+            if (isUndefined(input) || this._shouldSkip) {
+                if (!this._options.keepDataWhenSkipping) {
+                    this.setValue(undefined);
+                }
+                return Promise.resolve('Based on the input, SKIPPING this.');
             }
+            this._logger.log('Loading data for input', input);
+            if (!this._options.keepPreviousData) {
+                this.setValue(undefined);
+            }
+            const loadPromise = this._options.load(input);
+            loadPromise.then(
+                (result) => {
+                    this._logger.log('Loaded', result);
+                    this.setValue(result);
+                    const { onLoad } = this._options;
+                    if (onLoad) {
+                        onLoad(result);
+                    }
+                    this._scheduleNext();
+                },
+                (error) => {
+                    this._logger.log('Failed to load:', error);
+                    this._scheduleNext();
+                }
+            );
+            return loadPromise;
         },
     });
+
+    private _fetchIfWeHaveTo(): Promise<string | void> {
+        if (this._sleeping) {
+            this._logger.log('Sleeping, not fetching.');
+            return Promise.resolve('Sleeping, not fetching.');
+        }
+        if (this._relaxing) {
+            return Promise.resolve('Relaxing, not fetching.');
+        }
+        if (this._areWeUpToDate) {
+            return Promise.resolve('We are up to date already, not fetching.');
+        }
+        if (this._load.pending) {
+            console.log('Oops trying to call fetch while already pending on', this._options.id);
+            return Promise.resolve('Already loading'); // TODO: make sure that we will reload later
+        }
+        return this._load.firePromise();
+    }
+
+    /**
+     * Make sure that we are up-to-date!
+     *
+     *      * ... unless we are sleeping. If we are sleeping, do nothing.
+     */
+    private _update() {
+        if (this._sleeping) {
+            return;
+        }
+        if (this._relaxing) {
+            this._relaxing = false;
+            this._logger.log('Stop relaxing!');
+        }
+        void this._fetchIfWeHaveTo();
+    }
+
+    readonly refresh = new ActionImpl({
+        id: 'loaderActionRefresh',
+        fire: () => {
+            this._logger.log('Invalidating data');
+            this._fetchedForInput = undefined;
+            this._checkedAt = undefined;
+            return this._fetchIfWeHaveTo();
+        },
+    });
+
+    private _sleeping = true;
+
+    private _timer: NodeJS.Timeout | undefined;
 
     /**
      * Start to listen
      */
-    awaken() {
-        this.fetch.fire();
+    init() {
+        if (!this._sleeping) {
+            this._logger.log('Not awakening, not sleeping.');
+            return;
+        }
+        this._sleeping = false;
+        this._logger.log('Awakening. Relaxed?', !!this._options.initRelaxed);
+        if (this._options.initRelaxed) {
+            this.relax();
+        }
+        void this._fetchIfWeHaveTo();
         reaction(
             () => this.input,
-            () => this.fetch.fire()
+            () => this._fetchIfWeHaveTo()
         );
     }
 
@@ -82,14 +210,14 @@ export class LoaderImpl<Input, Output> implements Loader<Input, Output> {
     /**
      * Are we loading now?
      */
-    @computed
     get loading() {
-        return this.fetch.pending;
+        this._update();
+        return this._load.pending;
     }
 
     @computed
     get errorMessage() {
-        return this.fetch.errorMessage;
+        return this._load.errorMessage;
     }
 
     @observable
@@ -98,8 +226,8 @@ export class LoaderImpl<Input, Output> implements Loader<Input, Output> {
     /**
      * Return the value
      */
-    @computed
     get value() {
+        this._update();
         return this._value;
     }
 
